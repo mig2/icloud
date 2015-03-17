3e454a89a0161d771504fe8f5f8e90aac1a7c971
Matt Greenwood <matt@thegreenwoods.org>
2015-03-17 13:30:25 -0400

    Initial Commit - too late

---
+++ icloud.go
@@ -0,0 +1,23 @@
+package icloud
+
+// Types global to ICloud
+
+// Internal Functions
+
+// Not completely sure why I need this....
+
+// Functions from here on are exported
+
+// Functions exported on the ICloud type....
+
+/*
+
+func NewEngine(apple_id, password string) (*ICloudEngine, error) {
+	return engine.NewEngine(apple_id, password)
+}
+
+func RemindersApp(e *ICloudEngine) (*ICloudRemindersApp, error){
+	return reminders.NewApp(e)
+}
+
+*/
